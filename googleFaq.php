0defe8fca4ed403830f1a00c41a87e286d87885a
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-27 15:39:22 +0200

    fixed font error

--- googleFaq.php
+++ googleFaq.php
@@ -69,8 +69,8 @@ $faq = [
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <!-- link font -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com"   crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital, wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet"> 
 
   <link rel="stylesheet" href="css/style.css">
   <title>Google faq</title>
@@ -105,7 +105,7 @@ $faq = [
 </header>
 
 <main>
-  <div class="container">
+  <div class="container-lg">
 
     <?php
       foreach($faq as $singleFaq){ ?>
